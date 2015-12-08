65fce99bd3409d706693a37b0cf4530251a14a1d
Shawn Busolits <api.sbusolits@gmail.com>
2015-12-08 10:16:42 -0500

    Publishing latest changes

--- simple/ads.js
+++ simple/ads.js
@@ -44,12 +44,10 @@ function requestAds() {
 
   // Request video ads.
   var adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl =
-    'http://pubads.g.doubleclick.net/gampad/ads?sz=400x300&' +
-    'iu=%2F6062%2Fiab_vast_samples&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
-    'env=vp&output=vast&unviewed_position_start=1&url=' +
-    '[referrer_url]&correlator=[timestamp]&cust_params=iab_vast_samples' +
-    '%3Dlinear';
+  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+      'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
+      'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
+      'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
 
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
