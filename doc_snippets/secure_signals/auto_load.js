64c87a412241d6184e6b173362f46cf5e5044104
Jackson Sui <jsui@google.com>
2025-08-06 11:44:32 -0700

    Adds a snippet for the secure signals guide.
    PiperOrigin-RevId: 791779799

---
+++ doc_snippets/secure_signals/auto_load.js
@@ -0,0 +1,23 @@
+console.log("initializing IMA");
+adContainer = document.getElementById('ad-container');
+adDisplayContainer = new google.ima.AdDisplayContainer(adContainer, videoElement);
+
+const NETWORK_CODE = '12345678901';
+
+/**
+ * Sample signal collector function.
+ * @return {!Promise<string>} A promise that resolves to the encoded signals.
+ */
+const signalCollector = () => {
+ return new Promise((resolve, reject) => {
+   resolve("My encoded signal string");
+ });
+};
+if (!googletag) googletag = {};
+if (!googletag.secureSignalProviders) googletag.secureSignalProviders = [];
+googletag.secureSignalProviders.push({
+ networkCode: NETWORK_CODE,
+ collectorFunction: signalCollector
+});
+
+adsLoader = new google.ima.AdsLoader(adDisplayContainer);
